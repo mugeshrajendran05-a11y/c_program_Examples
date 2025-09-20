2de1ac15798fb478c361795c4f4f5a18e07d9188
mugeshrajendran05-a11y <mugeshrajendran05@gmail.com>
2025-09-20 14:41:05 +0530

    Add files via upload

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int n,count=1;
+	scanf("%d\n",&n);
+	char str[n];
+	scanf("%[^\n]",str);
+	for (int i=0; str[i]!= '\0';i++){
+		if(str[i]== '')
+	        count++;
+}
+else
+printf("%d",count);
+return 0;
+}
+	
